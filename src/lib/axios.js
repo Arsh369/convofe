af449d81b03ab8e4e982d568609dc74719e22088
Arsh369 <playgames1797@gmail.com>
2025-05-16 17:22:49 +0530

    commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  baseURL: "https://convobe.onrender.com/api",
+  baseURL: "/api",
   withCredentials: true,
 });
